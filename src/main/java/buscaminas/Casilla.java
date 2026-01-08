0289a3494d7c429726b62f7b64a79f02d54b8c5f
Mykola Peredrii <pkeorley@gmail.com>
2026-01-08 21:20:56 +0100

    refactor: remover atributo `blanco`

--- src/main/java/buscaminas/Casilla.java
+++ src/main/java/buscaminas/Casilla.java
@@ -1,79 +1,70 @@
 package buscaminas;
 
 public class Casilla {
-    private boolean mina;
-    private boolean blanco;
-    private boolean bandera;
-    private boolean visible;
-    private int numero;
+	private boolean mina;
+	private boolean bandera;
+	private boolean visible;
+	private int numero;
 
-    public boolean isMina() {
-        return this.mina;
-    }
+	public boolean isMina() {
+		return this.mina;
+	}
 
-    public boolean isBlanco() {
-        return this.blanco;
-    }
+	public boolean isBandera() {
+		return this.bandera;
+	}
 
-    public boolean isBandera() {
-        return this.bandera;
-    }
+	public boolean isVisible() {
+		return this.visible;
+	}
 
-    public boolean isVisible() {
-        return this.visible;
-    }
+	public int getNumero() {
+		return this.numero;
+	}
 
-    public int getNumero() {
-        return this.numero;
-    }
+	public void setMina(boolean mina) {
+		this.mina = mina;
+	}
 
-    public void setMina(boolean mina) {
-        this.mina = mina;
-    }
+	public void setBandera(boolean bandera) {
+		this.bandera = bandera;
+	}
 
-    public void setBlanco(boolean blanco) {
-        this.blanco = blanco;
-    }
+	public void setVisible(boolean visible) {
+		this.visible = visible;
+	}
 
-    public void setBandera(boolean bandera) {
-        this.bandera = bandera;
-    }
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
 
-    public void setVisible(boolean visible) {
-        this.visible = visible;
-    }
+	/**
+	 * Las casillas se mostrarán por consola de la siguiente forma:
+	 * <p>
+	 * Casilla oculta: .
+	 * Casilla con número: se mostrará el número correspondiente
+	 * Casilla blanca (sin minas alrededor): espacio en blanco
+	 * Casilla con mina: M
+	 * Casilla con bandera: B
+	 */
+	@Override
+	public String toString() {
+		if (this.bandera) {
+			return "B";
+		}
+		if (!this.visible) {
+			return ".";
+		}
+		if (this.mina) {
+			return "M";
+		}
+		if (this.numero > 0) {
+			return String.valueOf(this.numero);
+		}
+		if (this.numero == 0) {
+			return " ";
+		}
 
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-
-    /**
-     * Las casillas se mostrarán por consola de la siguiente forma:
-     * <p>
-     * Casilla oculta: .
-     * Casilla con número: se mostrará el número correspondiente
-     * Casilla blanca (sin minas alrededor): espacio en blanco
-     * Casilla con mina: M
-     * Casilla con bandera: B
-     */
-    @Override
-    public String toString() {
-        if (this.bandera) {
-            return "B";
-        }
-        if (!this.visible) {
-            return ".";
-        }
-        if (this.mina) {
-            return "M";
-        }
-        if (this.numero > 0) {
-            return String.valueOf(this.numero);
-        }
-        if (this.blanco) {
-            return " ";
-        }
-
-        return ""; // Cadena vacía
-    }
+		return ""; // Cadena vacía
+	}
 }
